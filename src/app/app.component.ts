75a07717ec1a733b58f05e2d8aba318f9bc0285a
Pratibha <pratibhasutar6451@gmail.com>
2022-09-12 12:54:54 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  
   title = 'second-project';
+  name = "Pratibha";
+  enabled = false;
+
+ 
+
+  ngOnInit(): void {
+  setInterval(()=>{
+  this.enabled = !this.enabled;
+  }, 1000)
+}
+
+clicked(){
+  alert("You Clicked Button");
+}
+
+
+/*texttyped(event:Event){
+  let ctrl = <HTMLInputElement>event.target;
+  this.name = ctrl.value;
+  //alert("Entered Text")
+
+}*/
 }
